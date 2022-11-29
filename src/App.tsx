57fdf50bcf5991f74fd80b2578d338ac59ad2244
paranoiaqq <nikitabelousov@ymail.com>
2022-11-29 13:08:20 +0300

    Created PortfolioChart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import PortfolioChart from "./components/PortfolioChart/PortfolioChart";
 
 function App() {
   return (
     <>
       <div className="header-container">
         <Header />
+      </div>
+      <div className="container">
+        <PortfolioChart />
       </div>
     </>
   );
---
+++ src/components/PortfolioChart/PortfolioChart.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./PortfolioChart.scss";
+
+const labels = [
+  "Ноябрь",
+  "Январь 2022",
+  "Март",
+  "Май",
+  "Июль",
+  "Сентябрь",
+  "Ноябрь",
+];
+const data = [{}];
+
+function PortfolioChart() {
+  return <div>chart</div>;
+}
+
+export default PortfolioChart;
