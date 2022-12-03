9b7624b63f29231d8cb8c746ade327e959d56ef4
paranoiaqq <nikitabelousov@ymail.com>
2022-12-03 12:58:29 +0300

    Trying to fix clipPath

--- src/components/PortfolioChart/PortfolioChart.tsx
+++ src/components/PortfolioChart/PortfolioChart.tsx
@@ -2,78 +2,89 @@ import React, { useEffect, useState } from "react";
 import "./PortfolioChart.module.scss";
 import { BarChart, Bar, XAxis, ResponsiveContainer } from "recharts";
 
-type CustomBarProps = {
+interface CustomBarProps {
   fill?: string;
   x?: number;
   y?: number;
   width?: number;
   height?: number;
-};
+}
 
 const CustomBar = ({ fill, x, y, width, height }: CustomBarProps) => {
-  // return <rect fill={fill} width={width} height={height} x={x} y={y} rx={10} />;
-  console.log(x, y, width, height);
-
   return (
-    <path
-      d={`M ${x}, ${height}
-      v -${height}
-      a 10,10 0 0 0 0,0
-      h ${width}
-      a 10,10 0 0 0 0,0
-      v ${height}
-      z
-  `}
-    />
+    <svg>
+      <defs>
+        <clipPath id="round-corner">
+          <rect
+            fill={fill}
+            x={x}
+            y={y}
+            width={width}
+            height={height ? height + 10 : 0}
+            rx="10"
+            ry="10"
+          />
+        </clipPath>
+      </defs>
+
+      <rect
+        x={x}
+        y={y}
+        fill={fill}
+        width={width}
+        height={height}
+        clip-path="url(#round-corner)"
+      ></rect>
+    </svg>
   );
 };
 
 function PortfolioChart() {
   const [data, setData] = useState([
-    // {
-    //   name: "Январь",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Февраль",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Март",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Апрель",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Май",
-    //   value: "3000",
-    // },
-    // {
-    //   name: "Июнь",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Июль",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Август",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Сентябрь",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Октябрь",
-    //   value: "1000",
-    // },
-    // {
-    //   name: "Ноябрь",
-    //   value: "1000",
-    // },
+    {
+      name: "Январь",
+      value: "1000",
+    },
+    {
+      name: "Февраль",
+      value: "3000",
+    },
+    {
+      name: "Март",
+      value: "1000",
+    },
+    {
+      name: "Апрель",
+      value: "2750",
+    },
+    {
+      name: "Май",
+      value: "3000",
+    },
+    {
+      name: "Июнь",
+      value: "1000",
+    },
+    {
+      name: "Июль",
+      value: "1000",
+    },
+    {
+      name: "Август",
+      value: "1000",
+    },
+    {
+      name: "Сентябрь",
+      value: "1000",
+    },
+    {
+      name: "Октябрь",
+      value: "1000",
+    },
+    {
+      name: "Ноябрь",
+      value: "1000",
+    },
     {
       name: "Декабрь",
       value: "1000",
